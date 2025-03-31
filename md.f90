920cbf82bb351df75b586d0202487fcccbd4130a
austinl1905 <austinl@DESKTOP-RCELDJA>
2025-03-31 17:52:10 -0400

    add dump

--- md.f90
+++ md.f90
@@ -32,4 +32,42 @@ MODULE MD
             END DO
         RETURN
     END
+
+    SUBROUTINE DUMP(R, T, I)
+        CHARACTER(LEN = 20) :: FILENAME
+        REAL, DIMENSION(N, D) :: R
+        REAL :: T
+        INTEGER :: I, J, K
+
+        WRITE(FILENAME, '(A, I0, A)') 'dump/data', I, '.txt'
+
+        OPEN(1, FILE=FILENAME, STATUS = 'new')
+
+        WRITE(1, *) "ITEM: STEP"
+        WRITE(1, '(I10)') I
+        WRITE(1, *) "ITEM: DT"
+        WRITE(1, '(F5.2)') DT
+        WRITE(1, *) "ITEM: TIME"
+        WRITE(1, '(F5.2)') T
+        WRITE(1, *) "ITEM: N ATOMS"
+        WRITE(1, '(I10)') N
+        WRITE(1, *) "ITEM: BOX CONDITION"
+
+        IF (BC) THEN 
+            WRITE(1, *) "PERIODIC"
+        ELSE 
+            WRITE(1, *) "REFLECTIVE"
+        END IF
+
+        WRITE(1, *) "ITEM: BOX BOUNDS (CUBE)"
+        WRITE(1, '(F5.3, A, F7.3)') 0.00, " ", L
+
+        WRITE(1, *) "ITEM: X Y Z"
+        
+        DO J = 1, N
+            WRITE(1, '(F10.6, F10.6, F10.6)') R(J, 1), R(J, 2), R(J, 3)
+        END DO
+
+        CLOSE(1)
+    END
 END MODULE
