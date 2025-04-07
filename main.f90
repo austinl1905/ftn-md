cb888ce2d71ff511fca30455d7f58af825263e68
austinl1905 <austinl@DESKTOP-RCELDJA>
2025-04-06 22:26:03 -0400

    attempt to add energy calculation

--- main.f90
+++ main.f90
@@ -4,18 +4,13 @@ PROGRAM MAIN
     IMPLICIT NONE
     REAL, DIMENSION(N, D) :: R, V ! POSITION AND VELOCITY VECTORS
     REAL, DIMENSION(N - 1, D) :: TEMP
-    INTEGER :: I, J
+    INTEGER :: I, J, K
 
     CALL RANDOM_NUMBER(R)
     CALL RANDOM_NUMBER(V)
 
     R = R * L
     V = (V * 200) - 100
-
-    PRINT *, "POSITIONS:"
-    PRINT *, R
-    PRINT *, "VELOCITIES:"
-    PRINT *, V
     
     DO I = 0, 100
         PRINT '(A, F10.6)', "POSITION AT T = ", DT * I
@@ -25,6 +20,10 @@ PROGRAM MAIN
         PRINT '(A, F10.6)', "VELOCITY AT T = ", DT * I
         DO J = 1, N
             PRINT *, "N = ", J, V(J, :)
+        END DO
+        PRINT '(A, F10.6)', "POTENTIAL ENERGIES AT T = ", DT * I
+        DO J = 1, N
+            PRINT *, "N = ", J, LJPOT(R, J)
         END DO
         CALL DUMP(R, DT * I, I)
         CALL UPDATE(R, V)
