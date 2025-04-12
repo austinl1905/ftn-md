6bb565d17f6b8f20200f711d840853c23b95bdfb
austinl1905 <austinl@DESKTOP-RCELDJA>
2025-04-12 18:11:11 -0400

    add attractive forces between molecules

--- main.f90
+++ main.f90
@@ -2,7 +2,7 @@ PROGRAM MAIN
     USE PARAMETERS
     USE MD
     IMPLICIT NONE
-    REAL, DIMENSION(N, D) :: R, V ! POSITION AND VELOCITY VECTORS
+    REAL, DIMENSION(N, D) :: R, V, A ! POSITION AND VELOCITY VECTORS
     REAL, DIMENSION(N - 1, D) :: TEMP
     INTEGER :: I, J, K
 
@@ -12,7 +12,9 @@ PROGRAM MAIN
     R = R * L
     V = (V * 200) - 100
     
-    DO I = 0, 100
+    DO I = 1, 100
+        CALL UPDATEV(R, V, A)
+        CALL UPDATE(R, V)
         PRINT '(A, F10.6)', "POSITION AT T = ", DT * I
         DO J = 1, N
             PRINT *, "N = ", J, R(J, :)
@@ -21,11 +23,12 @@ PROGRAM MAIN
         DO J = 1, N
             PRINT *, "N = ", J, V(J, :)
         END DO
+        PRINT '(A, F10.6)', "ACCELERATION AT T = ", DT * I
+        DO J = 1, N
+            PRINT *, "N = ", J, A(J, :)
+        END DO
         CALL DUMP(R, DT * I, I)
-        CALL UPDATE(R, V)
     END DO
-
-
     
 
 END PROGRAM MAIN
