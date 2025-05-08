f0e59598e61f5407d9a4d9b66afff89d9dd56573
austinl1905 <austinl@LIUAU-EliteBookG3>
2025-05-08 09:50:40 -0400

    add vmd cli support

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+echo THE FOLLOWING DEPENDENCIES ARE REQUIRED: VMD, GFORTRAN
 gfortran main.f90 parameters.f90 md.f90 -o main
-echo SUCCESSFULLY COMPILED
+if [ $? -eq 0 ]; then
+    echo SUCCESSFULLY COMPILED
+else
+    echo COMPILATION FAILED.
+    exit 1
+fi
 rm -rf dump/*
 ./main
 if test -f trajectory.xyz; then
@@ -18,4 +24,5 @@ for file in $(awk 'FNR==2 {print $NF, FILENAME}' dump/acc*.txt | sort -n | awk '
 do
     cat "$file" >> accs.txt
 done
-echo TRAJECTORY FILE SUCCESSFULLY CREATED
+echo TRAJECTORY FILE SUCCESSFULLY CREATED, LOADING TRAJECTORY FILE IN VMD
+vmd -xyz trajectory.xyz
